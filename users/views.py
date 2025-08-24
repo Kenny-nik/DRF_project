3dc5492320c230b72c9c81f3c405360cf42a55f7
Nikita <nikkitos.pershin.2000@mail.ru>
2025-08-24 11:15:01 +0500

    ApiViews and urls

--- users/views.py
+++ users/views.py
@@ -1,9 +1,21 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from rest_framework.filters import OrderingFilter
+from rest_framework.permissions import AllowAny
 
 from .models import User, Payments
 from .serializers import UserSerializer, PaymentsSerializer
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
 
 
 class UserRetrieveAPIView(generics.RetrieveAPIView):
@@ -26,3 +38,7 @@ class PaymentsListApiView(generics.ListAPIView):
         "payment_method",
     )
     ordering_fields = ("payment_date",)
+
+
+class UserDestroyAPIView(generics.DestroyAPIView):
+    queryset = User.objects.all()
