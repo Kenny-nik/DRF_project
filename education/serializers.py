4322a77909656c6b302ef2dd6d89c0dab2778add
Nikita <nikkitos.pershin.2000@mail.ru>
2025-08-30 09:47:15 +0500

    update

--- education/serializers.py
+++ education/serializers.py
@@ -1,27 +1,29 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from rest_framework import serializers
-from education.models import Course, Lesson
-from education.validators import validate_url
+from education.models import Course, Lesson, Subscription
+from education.validators import UrlValidator
 
 
 class LessonSerializer(ModelSerializer):
-    video_url = serializers.URLField(validators=[validate_url])
 
     class Meta:
         model = Lesson
         fields = "__all__"
+        validators = [UrlValidator(field="video_url")]
 
 
 class CourseSerializer(ModelSerializer):
     lessons_count = SerializerMethodField()
     lessons = LessonSerializer(many=True, read_only=True)
-
     def get_lessons_count(self, course):
         return course.Уроки.count()
-
 
     class Meta:
         model = Course
         fields = ('id', 'title', 'preview', 'description', 'lessons_count', 'lessons')
 
 
+class SubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = "__all__"
